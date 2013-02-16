5a59176e8a504b50015614c8a9849b24401bf948
DouglasSherk <github@sherk.me>
2013-02-16 04:20:10 -0500

    Upload basic Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tessera::Application.config.secret_token = 'bf14ec425c69287276e8e2974aa385cab491bf0ba1c5bbfc5b78e8ee8e59432bcd693b8ee847bf11524ac12bbcc6d80025ef894ac51d4ce3706ccc4f242623ad'
